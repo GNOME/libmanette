937a94199ca887fd66a8b9e85304cdd190ca8f0f
Adrien Plazas <kekun.plazas@laposte.net>
2020-03-21 10:04:53 +0100

    Add the update-gamecontrollerdb.sh script
    This can be used to update gamecontrollerdb.

---
+++ tools/update-gamecontrollerdb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+OUTPUT=src/gamecontrollerdb
+
+ensure-valid-id () {
+  grep -E '^[0-9a-f]{32},'
+}
+
+footer () {
+  printf "\n" >> $OUTPUT
+}
+
+
+
+#Cleanup
+rm $OUTPUT
+
+# Add the SDL DB header
+printf "# Source: https://github.com/SDL-mirror/SDL/blob/master/src/joystick/SDL_gamecontrollerdb.h\n\n" >> $OUTPUT
+
+# Add the SDL DB
+curl https://raw.githubusercontent.com/SDL-mirror/SDL/master/src/joystick/SDL_gamecontrollerdb.h \
+  | awk '/LINUX/{flag=1;next}/endif/{flag=0}flag' \
+  | sed -n 's/.*"\(.*\)".*/\1/p' \
+  | ensure-valid-id | sort >> $OUTPUT
+
+footer
+
+# Add the GameControllerDB header
+printf "# Source: https://github.com/gabomdq/SDL_GameControllerDB/blob/master/gamecontrollerdb.txt\n\n" >> $OUTPUT
+
+# Add the GameControllerDB
+curl https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt \
+  | grep "platform:Linux" \
+  | ensure-valid-id | sort >> $OUTPUT
+
+footer
+
